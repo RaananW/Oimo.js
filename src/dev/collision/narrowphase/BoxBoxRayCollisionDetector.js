7acf4916ee60e8fe0432048275cac9f6bb6279d4
XavCo7 <xcossettini@rogers.com>
2015-12-26 14:27:48 -0500

    Fixed comment

--- src/dev/collision/narrowphase/BoxBoxRayCollisionDetector.js
+++ src/dev/collision/narrowphase/BoxBoxRayCollisionDetector.js
@@ -5,7 +5,6 @@ OIMO.BoxBoxRayCollisionDetector.prototype = Object.create(OIMO.CollisionDetector
 OIMO.BoxBoxRayCollisionDetector.prototype.constructor = OIMO.BoxBoxRayCollisionDetector;
 
 OIMO.BoxBoxRayCollisionDetector.prototype.detectCollision = function(sh1, sh2, manifold){
-	// Note: This detector only works with tetras.
 	var vec3_1 = new OIMO.Vector3().fromArray(sh1.position.elements);
 	var vec3_2 = new OIMO.Vector3().fromArray(sh1.position.elements);
 	var intersect = new OIMO.Ray(vec3_1, vec3_2.normalize()).intersectBox(sh1.aabb);
