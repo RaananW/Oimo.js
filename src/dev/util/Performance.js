e0c501407f2f5ad7e9a86f66b7e3f3ae50ec82f6
XavCo7 <xcossettini@rogers.com>
2015-12-27 13:52:57 -0500

    Beautified code

--- src/dev/util/Performance.js
+++ src/dev/util/Performance.js
@@ -1,11 +1,15 @@
+/**
+ * Class for saving performance data each step.
+ * @author lo-th
+ */
 OIMO.Performance = function(){
-    this.time_prev=0;
-    this.fpsint=0;
-    this.fps=0;
+	this.time_prev = 0;
+	this.fpsint = 0;
+	this.fps = 0;
 
-    this.broadPhaseTime=0;
-    this.narrowPhaseTime=0;
-    this.solvingTime=0;
-    this.updatingTime=0;
-    this.totalTime=0;
-}
+	this.broadPhaseTime = 0;
+	this.narrowPhaseTime = 0;
+	this.solvingTime = 0;
+	this.updatingTime = 0;
+	this.totalTime = 0;
+};
