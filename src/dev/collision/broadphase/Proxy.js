4d12a04482437ecff02786aa67c0a5ebf3b31f0a
XavCo7 <xcossettini@rogers.com>
2015-12-26 20:15:57 -0500

    Beautified code

--- src/dev/collision/broadphase/Proxy.js
+++ src/dev/collision/broadphase/Proxy.js
@@ -1,21 +1,17 @@
 /**
-* A proxy is used for broad-phase collecting pairs that can be colliding.
-*/
+ * A proxy is used for broad-phase collecting pairs that can be colliding.
+ * @author lo-th
+ */
 OIMO.Proxy = function(shape){
 	// The parent shape.
-    this.shape=shape;
-    // The axis-aligned bounding box.
-    this.aabb=shape.aabb;
+	this.shape = shape;
+
+	// The axis-aligned bounding box.
+	this.aabb = shape.aabb;
 };
-
 OIMO.Proxy.prototype = {
-
-    constructor: OIMO.Proxy,
-    
-    /**
-	* Update the proxy.
-	*/
-    update:function(){
-        throw new Error("Inheritance error.");
-    }
-}
+	constructor: OIMO.Proxy,
+	update: function(){
+		throw new Error("Inheritance error.");
+	}
+};
