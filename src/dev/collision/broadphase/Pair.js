5629d5c97041c37737f97fd8a410af4ae56fdd9e
XavCo7 <xcossettini@rogers.com>
2015-12-26 19:41:30 -0500

    Beautified code

--- src/dev/collision/broadphase/Pair.js
+++ src/dev/collision/broadphase/Pair.js
@@ -1,10 +1,12 @@
 /**
 * A pair of shapes that may collide.
+* Specified by the active broadphase.
 * @author saharan
 */
 OIMO.Pair = function(){
-	// The first shape.
-    this.shape1=null;
-    // The second shape.
-    this.shape2=null;
+	// The first shape
+	this.shape1 = null;
+
+    // The second shape
+	this.shape2 = null;
 };
