1896ecccd0ee2facc65cca2dfd2f001cfa6381dd
XavCo7 <xcossettini@rogers.com>
2015-12-26 20:31:19 -0500

    Beautified more code

--- src/dev/collision/broadphase/AABB.js
+++ src/dev/collision/broadphase/AABB.js
@@ -19,7 +19,7 @@ OIMO.AABB = function(minX, maxX, minY, maxY, minZ, maxZ){
 OIMO.AABB.prototype = {
     constructor: OIMO.AABB,
 
-    init:function(mnX, mxX, mnY, mxY, mnZ, mxZ ){
+    init: function(mnX, mxX, mnY, mxY, mnZ, mxZ ){
 		this.minX = mnX || 0;
 		this.maxX = mxX || 0;
 		this.minY = mnY || 0;
@@ -28,38 +28,39 @@ OIMO.AABB.prototype = {
 		this.maxZ = mxZ || 0;
     },
 
-    /**
-     * Set this AABB to the combined AABB of aabb1 and aabb2.
-     * @param aabb1
-     * @param aabb2
-     */
-    combine: function(aabb1, aabb2){
+	/**
+	 * Set this AABB to the combined AABB of aabb1 and aabb2.
+	 * @param aabb1
+	 * @param aabb2
+	 */
+	combine: function(aabb1, aabb2){
 		this.minX = (aabb1.minX < aabb2.minX) ? aabb1.minX : aabb2.minX;
 		this.maxX = (aabb1.maxX > aabb2.maxX) ? aabb1.maxX : aabb2.maxX;
 		this.minY = (aabb1.minY < aabb2.minY) ? aabb1.minY : aabb2.minY;
 		this.maxY = (aabb1.maxY > aabb2.maxY) ? aabb1.maxY : aabb2.maxY;
 		this.minZ = (aabb1.minZ < aabb2.minZ) ? aabb1.minZ : aabb2.minZ;
 		this.maxZ = (aabb1.maxZ > aabb2.maxZ) ? aabb1.maxZ : aabb2.maxZ;
-    },
-
-    /**
-     * Get the surface area.
-     * @return
-     */
-	surfaceArea:function(){
-		var h=this.maxY-this.minY;
-		var d=this.maxZ-this.minZ;
-		return 2*((this.maxX-this.minX)*(h+d)+h*d);
 	},
 
-    /**
-     * Get whether the AABB intersects with the point or not.
-     * @param x
-     * @param y
-     * @param z
-     * @return whether there was a collision or not
-     */
+	/**
+	 * Get the surface area.
+	 * @return the surface area
+	 */
+	surfaceArea: function(){
+		var h = this.maxY - this.minY;
+		var d = this.maxZ - this.minZ;
+
+		return 2 * ((this.maxX - this.minX) * (h + d) + h * d);
+	},
+
+	/**
+	 * Get whether the AABB intersects with the point or not.
+	 * @param x
+	 * @param y
+	 * @param z
+	 * @return whether there was a collision or not
+	 */
 	intersectsWithPoint: function(x, y, z){
 		return x >= this.minX && x <= this.maxX && y >= this.minY && y <= this.maxY && z >= this.minZ && z<= this.maxZ;
 	}
-}
+};
