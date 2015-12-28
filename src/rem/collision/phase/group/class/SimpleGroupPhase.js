ab48c34e2f301cb1e4078bb7bbc64e43d0a4140f
XavCo7 <xcossettini@rogers.com>
2015-12-28 12:38:50 -0500

    Create SimpleGroupPhase.js

---
+++ src/rem/collision/phase/group/class/SimpleGroupPhase.js
@@ -0,0 +1,5 @@
+OIMO.SimpleGroupPhase = function(detail){
+	OIMO.GroupPhase.call(this, detail);
+};
+OIMO.SimpleGroupPhase.prototype = Object.create(OIMO.GroupPhase.prototype);
+OIMO.SimpleGroupPhase.prototype.constructor = OIMO.SimpleGroupPhase;
