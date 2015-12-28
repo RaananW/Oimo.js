b4c62f13d55124a8970355dee2585660054bf7d4
XavCo7 <xcossettini@rogers.com>
2015-12-28 12:45:08 -0500

    Fixed code

--- src/rem/collision/phase/group/GroupPhase.js
+++ src/rem/collision/phase/group/GroupPhase.js
@@ -2,14 +2,12 @@
  * Class for a group phase implementation, a
  * phase before broad phase to narrow down even
  * more the collision checking.
+ * @author xprogram
  */
 OIMO.GroupPhase = function(detail){
+	OIMO.CollisionPhase.call(this);
+
 	this.groupSize = detail;
 };
-OIMO.GroupPhase.prototype = {
-	constructor: OIMO.GroupPhase,
-
-	computePairs: function(){
-		OIMO.err("GroupPhase", OIMO.ERR_INHERITANCE);
-	}
-};
+OIMO.GroupPhase.prototype = Object.create(OIMO.CollisionPhase.prototype);
+OIMO.GroupPhase.prototype.constructor = OIMO.GroupPhase;
