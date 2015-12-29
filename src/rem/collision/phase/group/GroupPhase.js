463647622435428ec6fb573ffb9ddd0afd7996f6
XavCo7 <xcossettini@rogers.com>
2015-12-29 14:32:01 -0500

    Put computing groups to the base class

--- src/rem/collision/phase/group/GroupPhase.js
+++ src/rem/collision/phase/group/GroupPhase.js
@@ -7,7 +7,59 @@
 OIMO.GroupPhase = function(detail){
 	OIMO.CollisionPhase.call(this);
 
+	this.groups = [];
 	this.groupSize = detail;
 };
 OIMO.GroupPhase.prototype = Object.create(OIMO.CollisionPhase.prototype);
 OIMO.GroupPhase.prototype.constructor = OIMO.GroupPhase;
+
+OIMO.GroupPhase.prototype.computeGroups = function(){
+	var i, j, m, c, ac, d, ad, f, bd, nr, pl, ck, gr, bodies = this.proxies;
+
+	i = bodies.length;
+	nr = []; // Array of distances
+	bd = []; // Array of AABBs to add to group
+	gr = []; // Array of proxy groups
+	pl = {}; // Object reference to AABBs from a distance
+
+	while(i--){
+		c = bodies[i];
+		ac = c.aabb;
+		j = bodies.length;
+		m = this.groupSize;
+
+		if(c.isGrouped)
+			continue;
+
+		bd.push(c);
+		while(j--){
+			d = bodies[j];
+			ad = d.aabb;
+			ck = ac.center.distanceTo(ad.center);
+
+			// Cannot collide with itself
+			if(c === d)
+				continue;
+
+			nr.push(ck);
+			pl[ck] = d; // Store index for later use
+		}
+
+		// Sort array to quickly get the nearest AABBs
+		nr.sort(OIMO.ARR_NUMBERS_UP);
+
+		// Add the nearest AABBs to the new group
+		while(m--){
+			if(!nr.length)
+				break;
+
+			f = pl[nr.shift()];
+			f.isGrouped = true;
+			bd.push(f);
+		}
+
+		gr.push(new OIMO.GroupProxy(bd));
+	}
+
+	this.groups = gr;
+};
