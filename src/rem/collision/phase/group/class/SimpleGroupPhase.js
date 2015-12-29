1d81487763eebeeeea7d48ce969439788f2786a2
XavCo7 <xcossettini@rogers.com>
2015-12-29 14:44:53 -0500

    Fixed code to respect inheritance

--- src/rem/collision/phase/group/class/SimpleGroupPhase.js
+++ src/rem/collision/phase/group/class/SimpleGroupPhase.js
@@ -11,58 +11,12 @@ OIMO.SimpleGroupPhase.prototype.removeProxy = function(proxy){
 	this.proxies.splice(this.proxies.indexOf(proxy), 1);	
 };
 OIMO.SimpleGroupPhase.prototype.computePairs = function(){
-	/*
-	 * What we are doing:
-	 * We are using an optimized version of brute force collision
-	 * detection to group and check AABB proxies merged together.
-	 * That's just the concept.
-	 */
-	var i, j, m, c, ac, d, ad, f, bd, nr, pl, ck, gr, bodies = this.proxies;
+	var i, j, c, d, gr;
 
-	i = bodies.length;
-	nr = []; // Array of distances
-	bd = []; // Array of AABBs to add to group
-	gr = []; // Array of proxy groups
-	pl = {}; // Object reference to AABBs from a distance
+	// Create groups with given shape proxies
+	this.computeGroups();
 
-	while(i--){
-		c = bodies[i];
-		ac = c.aabb;
-		j = bodies.length;
-		m = this.groupSize;
-
-		if(c.isGrouped)
-			continue;
-
-		bd.push(c);
-		while(j--){
-			d = bodies[j];
-			ad = d.aabb;
-			ck = ac.center.distanceTo(ad.center);
-
-			// Cannot collide with itself
-			if(c === d)
-				continue;
-
-			nr.push(ck);
-			pl[ck] = d; // Store index for later use
-		}
-
-		nr.sort(OIMO.ARR_NUMBERS_UP);
-
-		// Add the nearest AABBs to the new group
-		while(m--){
-			if(!nr.length)
-				break;
-
-			f = pl[nr.shift()];
-			f.isGrouped = true;
-			bd.push(f);
-		}
-
-		gr.push(new OIMO.GroupProxy(bd));
-	}
-
+	gr = this.groups;
 	i = gr.length;
 
 	// Check each group for collision (using brute-force)
@@ -73,7 +27,7 @@ OIMO.SimpleGroupPhase.prototype.computePairs = function(){
 		while(j--){
 			d = gr[j];
 
-			if(OIMO.GroupUtils.detectCollision(c, d))
+			if(this.isAvailablePair(c, d) && c.aabb.intersects(d.aabb))
 				this.addPair(c, d);
 		}
 	}
