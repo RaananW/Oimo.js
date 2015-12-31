21e59c06b1f7c9eb00189c11ae79f81ab23c3a1e
XavCo7 <xcossettini@rogers.com>
2015-12-31 17:49:13 -0500

    Added prototype

--- src/rem/collision/phase/broad/bf/BFBroadPhase.js
+++ src/rem/collision/phase/broad/bf/BFBroadPhase.js
@@ -5,6 +5,25 @@
  */
 OIMO.BFBroadPhase = function(){
 	OIMO.BroadPhase.call(this);
+};
+OIMO.BFBroadPhase.prototype = Object.create(OIMO.BroadPhase.prototype);
+OIMO.BFBroadPhase.prototype.constructor = OIMO.BFBroadPhase;
 
-	this.proxies = [];
+OIMO.BFBroadPhase.prototype.createProxy = function(shape){
+	return new OIMO.BRBroadProxy(shape);
+};
+OIMO.BFBroadPhase.prototype.addProxy = function(proxy){
+	if(proxy instanceof OIMO.BRBroadProxy && this.proxies.indexOf(proxy) !== -1)
+		this.proxies.push(proxy);
+};
+OIMO.BFBroadPhase.prototype.removeProxy = function(proxy){
+	var arr = this.proxies;
+	var i = arr.length;
+
+	while(i--){
+		if(arr[i] === proxy)
+			arr[i] = arr[arr.length - 1];
+	}
+
+	arr.length--;
 };
