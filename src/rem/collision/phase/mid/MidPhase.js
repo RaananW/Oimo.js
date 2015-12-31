c9b72d3b252ad1a734d783ed03b1dd23df72d741
XavCo7 <xcossettini@rogers.com>
2015-12-31 10:47:06 -0500

    Removed inheritance

--- src/rem/collision/phase/mid/MidPhase.js
+++ src/rem/collision/phase/mid/MidPhase.js
@@ -4,11 +4,28 @@
  * @author xprogram
  */
 OIMO.MidPhase = function(){
-	OIMO.CollisionPhase.call(this);
+	this.pairs = [];
 };
-OIMO.MidPhase.prototype = Object.create(OIMO.CollisionPhase.prototype);
-OIMO.MidPhase.prototype.constructor = OIMO.MidPhase;
+OIMO.BroadPhase.prototype = {
+	constructor: OIMO.MidPhase,
 
-OIMO.MidPhase.prototype.computePairs = function(){
-	OIMO.err("MidPhase", OIMO.ERR_INHERITANCE);
+	createProxy: function(){
+		OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
+	},
+	addProxy: function(){
+		OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
+	},
+	removeProxy: function(){
+		OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
+	},
+	computePairs: function(){
+		this.pairs = []; // Reset array
+		this.collectPairs();
+	},
+	collectPairs: function(){
+		OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
+	},
+	addPair: function(p1, p2){
+		this.pairs.push(new OIMO.Pair(p1, p2));
+	}
 };
