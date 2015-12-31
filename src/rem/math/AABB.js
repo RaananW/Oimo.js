c2e3bc9a9e6b3217ac902dfa58d70a38f4175b4c
XavCo7 <xcossettini@rogers.com>
2015-12-31 17:16:33 -0500

    Create AABB.js

---
+++ src/rem/math/AABB.js
@@ -0,0 +1,46 @@
+/**
+ * An axis-aligned bounding box.
+ * @author lo-th
+ * @author xprogram
+ */
+OIMO.AABB = function(){
+	this.min = new OIMO.Vec3;
+	this.max = new OIMO.Vec3;
+};
+OIMO.AABB.prototype = {
+	constructor: OIMO.AABB,
+
+	merge: function(aabb){
+		this.expandByPoint(aabb.min);
+		this.expandByPoint(aabb.max);
+		return this;
+	},
+	setFromPoints: function(pts){
+		var i = pts.length;
+
+		while(i--)
+			this.expandByPoint(pts[i]);
+
+		return this;
+	},
+	expandByPoint: function(pt){
+		this.min.min(pt);
+		this.max.max(pt);
+		return this;
+	},
+	expandByScalar: function(s){
+		this.min.addScalar(-s);
+		this.max.addScalar(s);
+		return this;
+	},
+	containsPoint: function(pt){
+		return (
+			pt.x >= this.min.x && pt.x <= this.max.x &&
+			pt.y >= this.min.y && pt.y <= this.max.y &&
+			pt.z >= this.min.z && pt.z <= this.max.z
+		);
+	},
+	containsBox: function(box){
+		return this.containsPoint(box.min) && this.containsPoint(box.max);
+	}
+};
