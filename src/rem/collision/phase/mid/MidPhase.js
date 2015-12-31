2564a03ec041f19f022c8fdf7f1fd90e1794c5fd
XavCo7 <xcossettini@rogers.com>
2015-12-31 10:50:00 -0500

    Fixed code

--- src/rem/collision/phase/mid/MidPhase.js
+++ src/rem/collision/phase/mid/MidPhase.js
@@ -6,24 +6,24 @@
 OIMO.MidPhase = function(){
 	this.pairs = [];
 };
-OIMO.BroadPhase.prototype = {
+OIMO.MidPhase.prototype = {
 	constructor: OIMO.MidPhase,
 
 	createProxy: function(){
-		OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
+		OIMO.err("MidPhase", OIMO.ERR_INHERITANCE);
 	},
 	addProxy: function(){
-		OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
+		OIMO.err("MidPhase", OIMO.ERR_INHERITANCE);
 	},
 	removeProxy: function(){
-		OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
+		OIMO.err("MidPhase", OIMO.ERR_INHERITANCE);
 	},
 	computePairs: function(){
 		this.pairs = []; // Reset array
 		this.collectPairs();
 	},
 	collectPairs: function(){
-		OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
+		OIMO.err("MidPhase", OIMO.ERR_INHERITANCE);
 	},
 	addPair: function(p1, p2){
 		this.pairs.push(new OIMO.Pair(p1, p2));
