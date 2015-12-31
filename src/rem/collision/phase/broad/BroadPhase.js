20da3add24faa9af9043230eab560112abb813a0
XavCo7 <xcossettini@rogers.com>
2015-12-31 10:39:44 -0500

    Added extra inheritance

--- src/rem/collision/phase/broad/BroadPhase.js
+++ src/rem/collision/phase/broad/BroadPhase.js
@@ -12,7 +12,20 @@ OIMO.BroadPhase = function(){
 OIMO.BroadPhase.prototype = Object.create(OIMO.CollisionPhase.prototype);
 OIMO.BroadPhase.prototype.constructor = OIMO.BroadPhase;
 
+OIMO.BroadPhase.prototype.createProxy = function(){
+	OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
+};
+OIMO.BroadPhase.prototype.addProxy = function(){
+	OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
+};
+OIMO.BroadPhase.prototype.removeProxy = function(){
+	OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
+};
 OIMO.BroadPhase.prototype.computePairs = function(){
+	this.pairs = []; // Reset array
+	this.collectPairs();
+};
+OIMO.BroadPhase.prototype.collectPairs = function(){
 	OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
 };
 OIMO.BroadPhase.prototype.addPair = function(p1, p2){
