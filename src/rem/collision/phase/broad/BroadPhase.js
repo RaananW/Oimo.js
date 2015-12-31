08c0b720c2a827aacff00a947d20bad0d3960fd2
XavCo7 <xcossettini@rogers.com>
2015-12-31 10:47:28 -0500

    Fixed semicolon errors

--- src/rem/collision/phase/broad/BroadPhase.js
+++ src/rem/collision/phase/broad/BroadPhase.js
@@ -12,21 +12,21 @@ OIMO.BroadPhase.prototype = {
 
 	createProxy: function(){
 		OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
-	};
+	},
 	addProxy: function(){
 		OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
-	};
+	},
 	removeProxy: function(){
 		OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
-	};
+	},
 	computePairs: function(){
 		this.pairs = []; // Reset array
 		this.collectPairs();
-	};
+	},
 	collectPairs: function(){
 		OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
-	};
+	},
 	addPair: function(p1, p2){
 		this.pairs.push(new OIMO.Pair(p1, p2));
-	};
+	}
 };
