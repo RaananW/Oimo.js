3ad01310d88fb309bcf10f67492ae3f0852adf1a
XavCo7 <xcossettini@rogers.com>
2015-12-31 11:31:42 -0500

    Fixed code

--- src/rem/collision/shape/Shape.js
+++ src/rem/collision/shape/Shape.js
@@ -6,30 +6,28 @@
 
 OIMO.Shape = function(config){
 	this.id = OIMO.id();
-	this.prev = null;
-	this.next = null;
-	this.proxy = null;
+
 	this.parent = null;
-	this.contactLink = null;
-	this.numContacts = 0;
+	this.contacts = [];
 	this.position = new OIMO.Vec3();
 	this.rotation = new OIMO.Mat33();
-	this.relativePosition = new OIMO.Vec3().copy(config.relativePosition);
-	this.relativeRotation = new OIMO.Mat33().copy(config.relativeRotation);
 	this.aabb = new OIMO.AABB();
+	this.proxy = null;
+
 	this.density = config.density;
 	this.friction = config.friction;
 	this.restitution = config.restitution;
 	this.belongsTo = config.belongsTo;
-	this.collidesWith = config.collidesWith;
+	this.hitIndex = config.hitIndex;
 };
 
 OIMO.Shape.prototype = {
 	constructor: OIMO.Shape,
-	calculateMassInfo: function(out){
-		OIMO.Error("Shape", OIMO.ERR_INHERITANCE);
+
+	setupMass: function(){
+		OIMO.err("Shape", OIMO.ERR_INHERITANCE);
 	},
 	updateProxy: function(){
-		OIMO.Error("Shape", OIMO.ERR_INHERITANCE);
+		this.proxy.setFromPoints(this.vertices).expandByScalar(OIMO.AABB_PROX);
 	}
 };
