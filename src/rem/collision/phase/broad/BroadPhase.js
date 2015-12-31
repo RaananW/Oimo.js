c02d11fd1b48efd935fee7cb2e9e092cab0db798
XavCo7 <xcossettini@rogers.com>
2015-12-31 10:45:11 -0500

    Removed inheritance

--- src/rem/collision/phase/broad/BroadPhase.js
+++ src/rem/collision/phase/broad/BroadPhase.js
@@ -5,29 +5,28 @@
  * @author xprogram
  */
 OIMO.BroadPhase = function(){
-	OIMO.CollisionPhase.call(this);
-
 	this.pairs = [];
 };
-OIMO.BroadPhase.prototype = Object.create(OIMO.CollisionPhase.prototype);
-OIMO.BroadPhase.prototype.constructor = OIMO.BroadPhase;
+OIMO.BroadPhase.prototype = {
+	constructor: OIMO.BroadPhase,
 
-OIMO.BroadPhase.prototype.createProxy = function(){
-	OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
-};
-OIMO.BroadPhase.prototype.addProxy = function(){
-	OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
-};
-OIMO.BroadPhase.prototype.removeProxy = function(){
-	OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
-};
-OIMO.BroadPhase.prototype.computePairs = function(){
-	this.pairs = []; // Reset array
-	this.collectPairs();
-};
-OIMO.BroadPhase.prototype.collectPairs = function(){
-	OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
-};
-OIMO.BroadPhase.prototype.addPair = function(p1, p2){
-	this.pairs.push(new OIMO.Pair(p1, p2));
+	createProxy: function(){
+		OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
+	};
+	addProxy: function(){
+		OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
+	};
+	removeProxy: function(){
+		OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
+	};
+	computePairs: function(){
+		this.pairs = []; // Reset array
+		this.collectPairs();
+	};
+	collectPairs: function(){
+		OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
+	};
+	addPair: function(p1, p2){
+		this.pairs.push(new OIMO.Pair(p1, p2));
+	};
 };
