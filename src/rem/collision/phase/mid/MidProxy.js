6253c6ea730a4d260d78b9ff27380a5d5a88b6ee
XavCo7 <xcossettini@rogers.com>
2016-01-04 18:53:39 -0500

    Delete MidProxy.js

--- src/rem/collision/phase/mid/MidProxy.js
+++
@@ -1,9 +0,0 @@
-/**
- * Class for a mid-phase proxy.
- * @author xprogram
- */
-OIMO.MidProxy = function(tri){
-	OIMO.Proxy.call(this, _aabbs.alloc().setFromVertices(tri));
-
-	this.triangle = tri;
-};
