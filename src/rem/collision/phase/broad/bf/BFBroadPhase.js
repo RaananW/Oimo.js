980b5de1f2fdb9759ad29c5e844cfbb3ab556637
XavCo7 <xcossettini@rogers.com>
2016-01-05 16:46:24 -0500

    Removed proxy usage

--- src/rem/collision/phase/broad/bf/BFBroadPhase.js
+++ src/rem/collision/phase/broad/bf/BFBroadPhase.js
@@ -2,6 +2,7 @@
  * A broad-phase algorithm with brute-force search.
  * This always checks for all possible pairs.
  * @author lo-th
+ * @author xprogram
  */
 OIMO.BFBroadPhase = function(){
 	OIMO.BroadPhase.call(this);
@@ -9,44 +10,24 @@ OIMO.BFBroadPhase = function(){
 OIMO.BFBroadPhase.prototype = Object.create(OIMO.BroadPhase.prototype);
 OIMO.BFBroadPhase.prototype.constructor = OIMO.BFBroadPhase;
 
-OIMO.BFBroadPhase.prototype.createProxy = function(shape){
-	return new OIMO.BRBroadProxy(shape);
-};
-OIMO.BFBroadPhase.prototype.addProxy = function(proxy){
-	if(proxy instanceof OIMO.BRBroadProxy && this.proxies.indexOf(proxy) !== -1)
-		this.proxies.push(proxy);
-};
-OIMO.BFBroadPhase.prototype.removeProxy = function(proxy){
-	var arr = this.proxies;
-	var i = arr.length;
-
-	while(i--){
-		if(arr[i] === proxy)
-			arr[i] = arr[arr.length - 1];
-	}
-
-	arr.length--;
-};
 OIMO.BRBroadPhase.prototype.collectPairs = function(){
-	var i, j, p1, b1, s1, p2, b2, s2, arr = this.proxies, l = arr.length;
+	var i, j, b1, s1, b2, s2, arr = this.shapes, l = arr.length;
 
 	this.numChecks = l * (l - 1) >> 1;
 
 	i = l; // Amount of proxies to check
 	while(i--){
-		p1 = arr[i]; // First proxy
-		b1 = p1.aabb; // AABB of proxy 1
-		s1 = p1.shape; // Shape of proxy 1
-		j = l; // Amount of proxies to check
+		s1 = arr[i]; // First shape
+		b1 = s1.aabb; // AABB of shape 1
+		j = l; // Amount of shapes to check
 
 		while(j--){
 			if(j !== i){
-				p2 = arr[j]; // Second proxy
-				b2 = p2.aabb; // AABB of proxy 2
-				s2 = p2.shape; // Shape of proxy 2
+				p2 = arr[j]; // Second shape
+				b2 = s2.aabb; // AABB of shape 2
 
 				if(b1.containsBox(b2) && this.isAvailablePair(s1, s2))
-					this.addPair(p1, p2);
+					this.addPair(s1, s2);
 			}
 		}
 	}
