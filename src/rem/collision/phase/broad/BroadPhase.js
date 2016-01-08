badded5230ba31a89ef8434de924c3f33ebaaeb3
XavCo7 <xcossettini@rogers.com>
2016-01-08 16:59:58 -0500

    Added option to choose from AABB or radius check

--- src/rem/collision/phase/broad/BroadPhase.js
+++ src/rem/collision/phase/broad/BroadPhase.js
@@ -5,6 +5,7 @@
  * @author xprogram
  */
 OIMO.BroadPhase = function(){
+	this.useBoundingBoxes = true; // Whether or not to use AABBs in broadphase
 	this.shapes = [];
 	this.pairs = [];
 	this.numChecks = 0;
@@ -14,10 +15,22 @@ OIMO.BroadPhase.prototype = {
 
 	computePairs: function(){
 		this.pairs = []; // Reset array
+		this.numChecks = 0; // Reset checks amount
 		this.collectPairs();
 	},
 	collectPairs: function(){
 		OIMO.err("BroadPhase", OIMO.ERR_INHERITANCE);
+	},
+	intersectTest: function(shape1, shape2){ // From cannon.js
+		if(this.useBoundingBoxes){
+			if(shape1.aabb.overlaps(shape2.aabb))
+				this.addPair(shape1, shape2);
+		} else {
+			if(shape1.position.distanceTo(shape2.position) <= shape2.boundingRadius * 2)
+				this.addPair(shape1, shape2);
+		}
+
+		this.numChecks++;
 	},
 	addPair: function(p1, p2){
 		this.pairs.push(new OIMO.Pair(p1, p2));
