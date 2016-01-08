ea653b56bf04598a7c1f89ff1dc6c0295243abed
XavCo7 <xcossettini@rogers.com>
2016-01-08 17:07:14 -0500

    Fixed code to respect inheritance

--- src/rem/collision/phase/broad/bf/BFBroadPhase.js
+++ src/rem/collision/phase/broad/bf/BFBroadPhase.js
@@ -11,23 +11,21 @@ OIMO.BFBroadPhase.prototype = Object.create(OIMO.BroadPhase.prototype);
 OIMO.BFBroadPhase.prototype.constructor = OIMO.BFBroadPhase;
 
 OIMO.BRBroadPhase.prototype.collectPairs = function(){
-	var i, j, b1, s1, b2, s2, arr = this.shapes, l = arr.length;
+	var i, j, s1, s2, arr = this.shapes, l = arr.length;
 
-	this.numChecks = l * (l - 1) >> 1;
+	//this.numChecks = l * (l - 1) >> 1;
 
 	i = l; // Amount of proxies to check
 	while(i--){
 		s1 = arr[i]; // First shape
-		b1 = s1.aabb; // AABB of shape 1
 		j = l; // Amount of shapes to check
 
 		while(j--){
 			if(j !== i){
-				p2 = arr[j]; // Second shape
-				b2 = s2.aabb; // AABB of shape 2
+				s2 = arr[j]; // Second shape
 
-				if(b1.containsBox(b2) && this.isAvailablePair(s1, s2))
-					this.addPair(s1, s2);
+				if(this.isAvailablePair(s1, s2))
+					this.intersectTest(s1, s2);
 			}
 		}
 	}
